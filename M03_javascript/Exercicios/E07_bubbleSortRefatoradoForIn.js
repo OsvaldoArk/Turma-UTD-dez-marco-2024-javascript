b755d64a55085e09517793eeba64f4ccaf57f0f6
Jonas Holanda <jonasholanda15@gmail.com>
2024-01-23 15:38:46 -0300

    refactor - refatorado o algoritmo bubble sort

---
+++ M03_javascript/Exercicios/E07_bubbleSortRefatoradoForIn.js
@@ -0,0 +1,57 @@
+function ordenaVetor(lista,ordem){
+
+    let vetor = lista;
+
+    for (let atual in vetor) {
+
+        for (let proximo in vetor) {
+            switch(ordem){ 
+                case 'crescente': 
+                    if(vetor[atual] < vetor[proximo]){
+                        trocaValores(vetor,atual,proximo);
+                    }
+                break;
+
+                case 'decrescente': 
+                    if(vetor[atual] > vetor[proximo]){
+                        trocaValores(vetor,atual,proximo);
+                    }
+                break;
+
+                // case 'inverso': 
+                //     trocaValores(vetor,atual,proximo);
+                // break;
+
+                default: 
+                    console.log('ordem não reconhecida');
+                
+            }
+        }
+
+    }
+
+    return vetor;
+
+
+}
+
+function trocaValores(vetor, atual, proximo) {
+    let temp = vetor[atual];
+    vetor[atual] = vetor[proximo];
+    vetor[proximo] = temp;
+
+    return vetor;
+}
+
+// Para os números aleatórios não utilizei o For In
+let numeros = [];
+
+for(let indice = 0; indice <6; indice++){
+    numeros[indice] = Math.round(Math.random()*99);
+}
+
+console.log("Vetor original: "+numeros);
+
+// console.log("Vetor inverso: "+ordenaVetor(numeros,'inverso'));
+console.log("Vetor em ordem crescente: "+ordenaVetor(numeros,'crescente'));
+console.log("Vetor em ordem decrescente: "+ordenaVetor(numeros,'decrescente'));
